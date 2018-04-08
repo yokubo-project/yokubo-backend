0503fa8897256fdd481218449e1498b25530f060
Markus Hösel <mail@hoeselm.at>
2018-04-08 20:04:39 +0200

    Moving polyfills, Adding polyfills test cases

--- src/scripts/db/dropTables.ts
+++ src/scripts/db/dropTables.ts
@@ -1,4 +1,4 @@
-import "../../api/polyfills";
+import "../shared/util/polyfills";
 
 import log from "../../shared/util/log";
 import sequelize from "../../shared/util/sequelize";
--- src/shared/util/polyfills.ts
+++ src/shared/util/polyfills.ts
@@ -4,18 +4,16 @@ import * as Sequelize from "sequelize";
 import * as sourceMapSupport from "source-map-support";
 import * as uuid from "uuid";
 
-import log from "../shared/util/log";
+import log from "./log";
 
 // initialize source map support for stack traces
 sourceMapSupport.install();
 
 // preserve data across async callbacks
 const clsNmespace = cls.createNamespace(`CLS_${uuid.v4()}`);
-// tslint:disable-next-line:no-unsafe-any
-(Sequelize as any).useCLS(clsNmespace);
+Sequelize.useCLS(clsNmespace);
 
-// tslint:disable-next-line
-const clsBluebird = require('cls-bluebird');
+const clsBluebird = require("cls-bluebird");
 clsBluebird(clsNmespace);
 
 global.Promise = bluebird;
@@ -27,5 +25,4 @@ fetch.Promise = bluebird;
 // make fetch available globally
 (global as any).fetch = require("node-fetch");
 
-// tslint:disable-next-line:no-console
-log.debug(`Polyfills and utils installed. CLS=${clsNmespace.name}`);
+log.info(`Polyfills and utils installed. CLS=${clsNmespace.name}`);
